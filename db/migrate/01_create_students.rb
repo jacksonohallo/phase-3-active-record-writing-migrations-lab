4b9db2cd16c0fdcaa39a961242a6521feae2c1eb
jacksonohallo <jacksonohallo@gmail.com>
2022-08-28 11:13:36 +0300

    done

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,13 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
+  
   def change
+    
+    create_table :students do |t|
+
+    t.string  :name
+    t.integer :grade
+    t.datetime :birthdate
+
+    end
   end
 end
